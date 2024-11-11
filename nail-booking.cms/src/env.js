cfe65aba35bb77772fe1c9f2e20173f9e17ca0f1
HelloKittty123 <trantrung2khp@gmail.con>
2024-11-11 21:24:05 +0700

    first commit

---
+++ nail-booking.cms/src/env.js
@@ -0,0 +1,7 @@
+(function (window) {
+  window.__env = window.__env || {};
+
+  // API url
+  // window.__env.apiUrl = 'http://cms.nhanpt.tech/';
+  window.__env.apiUrl = "https://api.pokeme.co.uk/";
+})(this);
